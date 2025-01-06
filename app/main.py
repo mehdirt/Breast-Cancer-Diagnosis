57368cf1b28e80f32a9b3f98f1cd9545545558a0
mehdirt <mahdirafati680@gmail.com>
2025-01-06 21:18:33 +0330

    Feat: Sidebar added alog with sliders

--- app/main.py
+++ app/main.py
@@ -2,8 +2,58 @@ import pandas as pd
 import streamlit as st
 import pickle
 
+from utils.data_utils import get_data
+
 def add_sidebar():
+    """"""
     st.sidebar.header("Cell Nuclei Measurements")
+
+    data = get_data(path="data/data.csv")
+
+    slider_labels = [
+        ("Radius (mean)", "radius_mean"),
+        ("Texture (mean)", "texture_mean"),
+        ("Perimeter (mean)", "perimeter_mean"),
+        ("Area (mean)", "area_mean"),
+        ("Smoothness (mean)", "smoothness_mean"),
+        ("Compactness (mean)", "compactness_mean"),
+        ("Concavity (mean)", "concavity_mean"),
+        ("Concave points (mean)", "concave points_mean"),
+        ("Symmetry (mean)", "symmetry_mean"),
+        ("Fractal dimension (mean)", "fractal_dimension_mean"),
+        ("Radius (se)", "radius_se"),
+        ("Texture (se)", "texture_se"),
+        ("Perimeter (se)", "perimeter_se"),
+        ("Area (se)", "area_se"),
+        ("Smoothness (se)", "smoothness_se"),
+        ("Compactness (se)", "compactness_se"),
+        ("Concavity (se)", "concavity_se"),
+        ("Concave points (se)", "concave points_se"),
+        ("Symmetry (se)", "symmetry_se"),
+        ("Fractal dimension (se)", "fractal_dimension_se"),
+        ("Radius (worst)", "radius_worst"),
+        ("Texture (worst)", "texture_worst"),
+        ("Perimeter (worst)", "perimeter_worst"),
+        ("Area (worst)", "area_worst"),
+        ("Smoothness (worst)", "smoothness_worst"),
+        ("Compactness (worst)", "compactness_worst"),
+        ("Concavity (worst)", "concavity_worst"),
+        ("Concave points (worst)", "concave points_worst"),
+        ("Symmetry (worst)", "symmetry_worst"),
+        ("Fractal dimension (worst)", "fractal_dimension_worst"),
+    ]
+
+    input_data = {}
+
+    for label, key in slider_labels:
+        input_data[key] = st.sidebar.slider(
+                label,
+                min_value=float(0),
+                max_value=float(data[key].max()),
+                value=float(data[key].mean()),
+            )
+    
+    return input_data
 
 def main():
     st.set_page_config(
@@ -13,7 +63,7 @@ def main():
         initial_sidebar_state="expanded"
     )
 
-    add_sidebar()
+    input_data = add_sidebar()
 
     with st.container():
         st.title("Breast Cancer Predictor")
