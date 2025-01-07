67e627205279f1b1b26ea8d81e310480b7c18473
mehdirt <mahdirafati680@gmail.com>
2025-01-07 12:31:20 +0330

    Feat: Css style aded to right columns

--- app/main.py
+++ app/main.py
@@ -176,6 +176,9 @@ def main():
         initial_sidebar_state="expanded"
     )
 
+    with open("assets/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
     input_data = add_sidebar()
 
     with st.container():
